20132cb12bafdcc685c7cf616a4852288bc377a1
makalex74 <makalex74@yandex.ru>
2024-01-13 13:06:10 +0300

    Like and Dislike buttons

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div>
+        <div>
+            <button v-on:click="addLike">Like</button>
+            <button v-on:click="addDislike">Dislike</button>
+        </div>
+        <div>Колич-во лайков: <strong>{{ likes }}</strong></div>
+        <div>Колич-во дизлайков: <strong>{{ dislikes }}</strong></div>
+    </div>
 </template>
-
+  
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+        }
+    },
+    methods: {
+        addLike(){
+            this.likes ++
+        },
+        addDislike(){
+            this.dislikes ++
+        }
+    }
+}
+</script>
+  
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
