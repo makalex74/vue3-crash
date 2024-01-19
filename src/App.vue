4f6712573dfc1f8c574d1f7a9ee421acbf7f0acb
makaenko_aa <makaenko_aa@magnit.ru>
2024-01-19 17:34:11 +0300

    Import axios. Add temp button fetchPosts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="app">
         <h1>Страница с постами</h1>
+        <my-button @click="fetchPosts" style="margin: 15px 0;">
+            Получить посты
+        </my-button>
         <my-button @click="showDialog" style="margin: 15px 0;">
             Создать пост
         </my-button>
@@ -14,6 +17,7 @@
 <script>
 import PostForm from './components/PostForm'
 import PostList from '@/components/PostList'
+import axios from 'axios';
 export default {
     components: {
         'post-form': PostForm, 
@@ -21,12 +25,7 @@ export default {
     }, 
     data() {
         return {
-            posts: [
-                { id: 1, title: 'JavaScript', body: 'Описание поста' },
-                { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
-                { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' },
-                { id: 4, title: 'JavaScript 4', body: 'Описание поста 4' },
-            ],
+            posts: [],
             dialogVisible: false,
         }
     },
@@ -40,6 +39,16 @@ export default {
         },
         showDialog() {
             this.dialogVisible = true;
+        },
+        async fetchPosts() {
+            try{
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                this.posts = response.data;
+                console.log(response);
+            } catch (e) {
+                alert('Ошибка');
+
+            }
         }
 
     }
