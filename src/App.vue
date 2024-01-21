a73375b12cbce62fcf37d6e8f46d8786cdb11fd7
makaenko_aa <makaenko_aa@magnit.ru>
2024-01-21 19:53:37 +0300

    find posts, filtration

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="app">
         <h1>Страница с постами</h1>
+        <my-input
+            v-model="searchQuery"
+            placeholder="Поиск..."
+        >            
+        </my-input>
         <div class="app__btns">
             <my-button @click="showDialog">
                 Создать пост
@@ -14,7 +19,7 @@
             <post-form  @create="createPost"/>
         </my-dialog>
         <post-list 
-             :posts="sortedPosts" 
+             :posts="sortedAndSearchedPosts" 
              @remove="removePost"
              v-if="!isPostsLoading"
         />
@@ -37,6 +42,7 @@ export default {
             dialogVisible: false,
             isPostsLoading: false,
             selectedSort: '',
+            searchQuery: '',
             sortOptions: [
                 {value: 'title', name: 'По названию'},
                 {value: 'body', name: 'По содержимому'},
@@ -73,6 +79,9 @@ export default {
         sortedPosts() {
             return [...this.posts].sort((post1, post2) => 
             post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+        },
+        sortedAndSearchedPosts() {
+            return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
         }
     },
     watch: {
