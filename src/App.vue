7e4ba2ca8a31ce2bb4ec2e01a9698490b531d292
makaenko_aa <makaenko_aa@magnit.ru>
2024-01-21 22:42:05 +0300

    Pagination. Page output.

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,19 @@
              v-if="!isPostsLoading"
         />
         <div v-else>Идет загрузка...</div>
+        <div class="page__wrapper">
+            <div 
+                v-for="pageNumber in totalPages" 
+                :key="pageNumber"
+                class="page"
+                :class="{
+                    'current-page': page === pageNumber
+                }"
+                @click="changePage(pageNumber)"
+            >
+                {{ pageNumber }}
+            </div>
+        </div>
     </div>
 </template>
   
@@ -43,6 +56,9 @@ export default {
             isPostsLoading: false,
             selectedSort: '',
             searchQuery: '',
+            page: 1,
+            limit: 10,
+            totalPages: 0,
             sortOptions: [
                 {value: 'title', name: 'По названию'},
                 {value: 'body', name: 'По содержимому'},
@@ -60,17 +76,26 @@ export default {
         showDialog() {
             this.dialogVisible = true;
         },
+        changePage(pageNumber) {
+            this.page = pageNumber;
+        },
         async fetchPosts() {
             try{
                 this.isPostsLoading = true;
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                    params: {
+                        _page: this.page,
+                        _limit: this.limit
+                    }
+                });
+                this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
                 this.posts = response.data;
             } catch (e) {
                 alert('Ошибка');
             } finally {
                 this.isPostsLoading = false;                
             }    
-        }
+        },
     },
     mounted() {
         this.fetchPosts();
@@ -90,6 +115,9 @@ export default {
         //         return post1[newValue]?.localeCompare(post2[newValue])
         //     })
         // }
+        page() {
+            this.fetchPosts();
+        }    
     } 
 }
 </script>
@@ -111,5 +139,18 @@ export default {
     justify-content: space-between;
 }
 
+.page__wrapper {
+    display: flex;
+    margin-top: 15px;
+}
+
+.page {
+    border: 1px solid black;
+    padding: 10px;
+}
+
+.current-page {
+    border: 2px solid teal ;
+}
 
 </style>
